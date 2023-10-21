d915a62a3ffdaf56c27e32d89ebf47ca44d2a043
Artem <artem555525fssg@gmail.com>
2023-10-22 02:37:23 +0300

    add test_1.py and add decision

--- test_1.py
+++ test_1.py
@@ -4,4 +4,26 @@
 # Один и тот же элемент нельзя использовать дважды.
 # Результат можно вернуть в любом порядке.
 
+#Сложность алгоритма O(n)
+#Идея алгоритма: x+y = target ,
+# если target - y = x и разница в виде ключа есть в словаре,
+# то при сложении y + dict[x] = target.
+# поиск по ключу в словаре реализуется за O(1)
+# самый плохой исход по сложности работы алгоритма,если мы пройдемся
+# по всему списку  за O(n)
+
+from typing import List
+
+
+def solve(nums: List[int], target: int) -> List[int]:
+    difference_dict = {}
+    for i, num in enumerate(nums):
+        difference = target - num
+        if difference in difference_dict:
+            return [difference_dict[difference], i]
+        difference_dict[num] = i
+    return list()
+
+#пример
+print(solve([3, 3], target=7))
 
